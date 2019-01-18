40ca91c8afaecfc433785eea3fc6f74445ade20b
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-01-17 20:00:27 -0800

    input value now respond to user input and appear in browser window

--- main.js
+++ main.js
@@ -2,21 +2,20 @@
 function greet() {
     
 // get the inputs by id
-var firstNameInput = document.getElementById('first-name');
-var lastNameInput = document.getElementById('last-name');
+    var firstNameInput = document.getElementById('first-name');
+    var lastNameInput = document.getElementById('last-name');
 
 //get values out of inputs
-var firstName = firstNameInput.value;
-var lastName = lastNameInput.value;
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
 
 
 // make the greeting 
-var greeting = 'Hello' + firstName + lastName;
+    var greeting = 'Hello ' + firstName + ' ' + lastName + '!';
 
-console.log(greeting);
 // display greeting
-
-
+    var greeter = document.getElementById('greeter');
+    greeter.textContent = greeting;
 
 // remove prompt
 
